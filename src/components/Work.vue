8f7f479c478bb7bff7b506cd4d0221630c528cbb
hanscristian3799 <hanscristian376@gmail.com>
2022-03-01 11:07:21 +0700

    align our work section

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -8,29 +8,32 @@
         and inclusive future to play a crucial role in and beyond the country.
       </h3>
     </div>
-    <div class="">
-      <div class="row text-2">
+    <div class="d-flex flex-row justify-content-between align-items-start">
+      <div class="text-2 w-50 me-5">
         <h2 class="font-30 mb-3 fw-bold">Blockchain Technology</h2>
-        <div class="col-2">
-          <p class="font-25">Kriptosh</p>
-          <p class="font-25">Kriptoklub</p>
-          <p class="font-25">Moments</p>
-        </div>
-        <div class="col-4">
-          <p class="font-25">Digital Asset Exchange</p>
-          <p class="font-25">Cyptocurrency Community</p>
-          <p class="font-25">NFT Marketplace</p>
+        <hr class="me-5" />
+
+        <div class="d-flex flex-row justify-content-between">
+          <div class="me-5">
+            <p class="font-25">Kriptosh</p>
+            <p class="font-25">Kriptoklub</p>
+            <p class="font-25">Moments</p>
+          </div>
+
+          <div class="me-5 flex-grow-1 ms-5">
+            <p class="font-25">Digital Asset Exchange</p>
+            <p class="font-25">Cyptocurrency Community</p>
+            <p class="font-25">NFT Marketplace</p>
+          </div>
         </div>
       </div>
-      <div class="row">
-        <div class="col-4">
-          <h2 class="font-30 fw-bold">Food & Beverages</h2>
-        </div>
-        <div class="col-2">
-          <p class="font-25">Vrew Coffee</p>
-        </div>
-        <div class="col-4">
-          <p class="font-25">Premium Coffee Shop</p>
+      <div class="text-2 w-50 ms-5">
+        <h2 class="font-30 fw-bold">Food & Beverages</h2>
+        <hr class="me-5" />
+
+        <div class="d-flex flex-row justify-content-between">
+          <p class="font-25 me-5">Vrew Coffee</p>
+          <p class="font-25 flex-grow-1 me-5 ms-5">Premium Coffee Shop</p>
         </div>
       </div>
     </div>
